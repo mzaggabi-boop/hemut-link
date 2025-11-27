2890fae62efa96a5a388321ccbf5264ac49dab7d
batilinkdev <mzaggabi@gmail.com>
2025-11-27 12:04:49 +0100

    Fix Button import path in stripe onboard page

--- frontend/app/payements/onboard/page.tsx
+++ frontend/app/payements/onboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import Button from "../../components/Button";
+import Button from "../../../components/Button";
 
 export default function StripeOnboardCallbackPage() {
   const router = useRouter();
