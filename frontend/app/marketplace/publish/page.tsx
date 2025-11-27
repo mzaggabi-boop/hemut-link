dd6063df13bf313d46a0119cc04a4c35ed3b45cd
batilinkdev <mzaggabi@gmail.com>
2025-11-27 12:56:11 +0100

    Fix Input and Button imports in marketplace publish page

--- frontend/app/marketplace/publish/page.tsx
+++ frontend/app/marketplace/publish/page.tsx
@@ -3,8 +3,9 @@
 
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
-import Input from "../../components/Input";
-import Button from "../../components/Button";
+import Input from "../../../components/Input";
+import Button from "../../../components/Button";
+
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:8081";
 
