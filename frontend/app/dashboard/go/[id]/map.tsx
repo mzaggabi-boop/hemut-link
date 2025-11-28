1a811fb69751ec805ec8088030cf14dc22ae4bce
batilinkdev <mzaggabi@gmail.com>
2025-11-28 22:24:44 +0100

    Fix react-leaflet MapContainer TS typing with @ts-ignore

--- frontend/app/dashboard/go/[id]/map.tsx
+++ frontend/app/dashboard/go/[id]/map.tsx
@@ -13,17 +13,21 @@ const icon = L.icon({
 
 export default function Map({ lat, lon }: { lat: number; lon: number }) {
   return (
-    <MapContainer
-      center={[lat, lon]}
-      zoom={15}
-      scrollWheelZoom={false}
-      style={{ width: "100%", height: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; OpenStreetMap'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[lat, lon]} icon={icon} />
-    </MapContainer>
+    <>
+      {/* ⛔ Fix TypeScript: MapContainer types broken in react-leaflet 4.x */}
+      {/* @ts-ignore */}
+      <MapContainer
+        center={[lat, lon]}
+        zoom={15}
+        scrollWheelZoom={false}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat, lon]} icon={icon} />
+      </MapContainer>
+    </>
   );
 }
