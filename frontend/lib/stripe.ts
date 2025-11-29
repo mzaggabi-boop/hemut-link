0dcbbdd118ebff54f5925a61a94ac383839bade3
batilinkdev <mzaggabi@gmail.com>
2025-11-30 00:02:37 +0100

    fix: stripe initialization

--- frontend/lib/stripe.ts
+++ frontend/lib/stripe.ts
@@ -1,12 +1,10 @@
-// lib/stripe.ts
-
-
-
 import Stripe from "stripe";
 
-// Stripe demande une apiVersion explicite avec Next.js 16
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-11-17.clover" as Stripe.LatestApiVersion,
-});
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Missing STRIPE_SECRET_KEY");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default stripe;
+
