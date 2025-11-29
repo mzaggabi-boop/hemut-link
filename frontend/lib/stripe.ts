edef6daf242297ff7b6f9b5935e29f303d9976ec
batilinkdev <mzaggabi@gmail.com>
2025-11-29 23:21:15 +0100

    Fix: stripe client runtime nodejs

--- frontend/lib/stripe.ts
+++ frontend/lib/stripe.ts
@@ -1,9 +1,9 @@
 // lib/stripe.ts
 
+export const runtime = "nodejs";
+
 import Stripe from "stripe";
 
-// ✔️ NE PAS définir apiVersion → Next.js 16 plante sinon
-// Stripe utilise automatiquement la version de ton dashboard
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export default stripe;
